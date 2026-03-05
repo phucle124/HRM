f81223e20023ed24eaf41b15236ac21b33b4cef8
phucle124 <phuc95721@gmail.com>
2026-03-06 00:31:54 +0700

    BackEnd test db

---
+++ BE/src/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const webRoute = require('./routes/web');
+
+const db = require('./config/db');
+
+const {getAllUsers} = require('./services/CRUDService')
+
+//config app
+const app = express();
+
+const port = process.env.PORT;
+const host = process.env.HOST_NAME;
+
+
+const testDb = async ()=>{
+    const KQ = await getAllUsers();
+    console.log(KQ);
+}
+
+testDb();
+
+app.listen(port,host, () =>{
+    console.log(`Server is now running on port ${port}`);
+});
+
+
