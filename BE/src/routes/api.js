92fbbf912a8d34c96b014199aa41428ab5b4c328
phucle124 <phuc95721@gmail.com>
2026-04-07 01:05:59 +0700

    tam thoi tat authorize

--- BE/src/routes/api.js
+++ BE/src/routes/api.js
@@ -19,10 +19,10 @@ const checkRole = require('../middlewares/Authorize');
 
 
 //Áp dụng middleware phân quyền cho API login,home,logout
-router.use((req, res, next) => {
-    if (req.path === '/login' || req.path === '/' || req.path === '/logout') return next();
-    checkRole(req, res, next);
-});
+// router.use((req, res, next) => {
+//     if (req.path === '/login' || req.path === '/' || req.path === '/logout') return next();
+//     checkRole(req, res, next);
+// });
 
 // --- DANH SÁCH API ---
 
