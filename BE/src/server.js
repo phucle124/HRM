bdf3e0d4d5719d8bbe254c951b51b0b00f4ada24
phucle124 <phuc95721@gmail.com>
2026-04-08 06:50:12 +0700

    commit BE lan cuoi doi cors Public url FE

--- BE/src/server.js
+++ BE/src/server.js
@@ -22,8 +22,8 @@ app.set('trust proxy', 1); //Giúp express tin tưởng vào proxy này
 
 //MiddleWares
 app.use(cors({
-    //origin: 'https://hrm-fe.42web.io', // public url
-    origin: 'http://localhost:5173',
+    origin: 'https://hrm-fe.42web.io', // public url
+    //origin: 'http://localhost:5173',
     credentials: true                // Cho phép gửi Session/Cookie
 }));
 app.use(sessionConfig);
