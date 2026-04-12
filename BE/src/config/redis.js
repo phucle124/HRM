ccfd95a2db955117a0216082a891938269e258cf
phucle124 <phuc95721@gmail.com>
2026-04-12 20:38:14 +0700

    refactor dung redis cloud thay vi redis local

--- BE/src/config/redis.js
+++ BE/src/config/redis.js
@@ -1,6 +1,8 @@
 const redis = require('redis');
+require('dotenv').config();
+
 const client = redis.createClient({
-    url: 'redis://localhost:6379'
+    url: process.env.REDIS_URL
 });
 client.on('error', err => console.log('Redis Client Error', err));
 client.connect().then(() => console.log('==> Redis Connected!'));
