5d4ad13810c32478ad51f3eff24a033466e54ceb
phucle124 <phuc95721@gmail.com>
2026-04-15 01:15:29 +0700

    commit lan cuoi

--- BE/src/server.js
+++ BE/src/server.js
@@ -22,10 +22,8 @@ app.set('trust proxy', 1); //Giúp express tin tưởng vào proxy này
 
 //MiddleWares
 app.use(cors({
-    //origin: 'https://hrm-fe.42web.io/', // public url
-    origin: ['http://localhost:5173',
-            'https://hrm-fe.42web.io/'
-    ],
+    origin: 'https://hrm-fe.42web.io/', // public url
+   
     credentials: true                // Cho phép gửi Session/Cookie
 }));
 app.use(sessionConfig);
